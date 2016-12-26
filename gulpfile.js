e7056ce7c55b32fcab8bfa433f34ecdc40132d48
Diogo Moretti <diogo.pearljam@gmail.com>
2016-12-26 14:48:17 -0200

    Add website and demo

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,14 @@ require('gulp-release-tasks')(gulp)
 
 gulp.task('connect', () => {
   connect.server({
-    root: './',
+    root: './_website',
     port: 7000,
     livereload: true
   })
 })
 
 gulp.task('html', () => {
-  gulp.src('./example/index.html')
+  gulp.src(['./_website/*.html', './_website/demo/*.html'])
     .pipe(connect.reload())
 })
 
@@ -29,6 +29,12 @@ gulp.task('minify', () => {
     .pipe(gulp.dest('./src'))
 })
 
+
+gulp.task('copy', () => {
+  gulp.src('./src/snakke.min.js')
+    .pipe(gulp.dest('./_website/demo/'))
+})
+
 gulp.task('standard', () => {
   gulp.src(['./src/snakke.js'])
     .pipe(standard())
@@ -40,14 +46,14 @@ gulp.task('standard', () => {
 })
 
 gulp.task('watch', () => {
-  gulp.watch('./src/snakke.js', ['standard', 'html', 'minify'])
-  gulp.watch('./example/*.{html,css}', ['html'])
+  gulp.watch('./src/snakke.js', ['build'])
+  gulp.watch('./_website/**/*.{html,css}', ['html'])
 })
 
 gulp.task('build', () => {
-  runSequence('standard', 'minify')
+  runSequence('standard', 'minify', 'copy')
 })
 
 gulp.task('server', () => {
-  runSequence('standard', 'connect', 'watch')
+  runSequence('build', 'connect', 'watch')
 })
