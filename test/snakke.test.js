5228f68c39b5679649c5d85afc5b059f20cf2ef1
Diogo Moretti <diogo.pearljam@gmail.com>
2017-07-28 14:56:36 -0300

    Test file is standard

--- test/snakke.test.js
+++ test/snakke.test.js
@@ -1,11 +1,11 @@
-import { Selector } from 'testcafe';
+import { Selector } from 'testcafe'
 
 fixture `Snakke E2E Test`
-  .page `../docs/demo/index.html`;
+  .page `../docs/demo/index.html`
 
 test('Check if div snakke-bar is inserted', async t => {
-  const snakkeDiv = Selector('.snakke-bar').exists;
+  const snakkeDiv = Selector('.snakke-bar').exists
 
   await t
-    .expect(snakkeDiv).ok();
-});
+    .expect(snakkeDiv).ok()
+})
